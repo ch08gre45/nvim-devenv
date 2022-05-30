ce45b8dca3a0a8839282cb0936dbbd800324cefd
ch08gre45 <drazvan85@gmail.com>
2022-05-30 23:43:34 +0300

    Treesitter changes and some vim backwards incompatible QOL configs

--- nvim/lua/_treesitter.lua
+++ nvim/lua/_treesitter.lua
@@ -6,6 +6,32 @@ require'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
+    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+        -- For all filetypes
+        -- Note that setting an entry here replaces all other patterns for this entry.
+        -- By setting the 'default' entry below, you can control which nodes you want to
+        -- appear in the context window.
+        default = {
+            "function",
+            "method",
+            "for",
+            "while",
+            "if",
+            "switch",
+            "case",
+        },
+
+        rust = {
+            "loop_expression",
+            "impl_item",
+        },
+
+        typescript = {
+            "class_declaration",
+            "abstract_class_declaration",
+            "else_clause",
+        },
+    },
   },
   incremental_selection = {
     enable = true,
