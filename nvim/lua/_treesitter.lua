f51b24d93a3f64f1a0ba4a405f8684307b9fef11
ch08gre45 <drazvan85@gmail.com>
2022-06-14 12:39:20 +0300

    Treesitter configs

--- nvim/lua/_treesitter.lua
+++ nvim/lua/_treesitter.lua
@@ -7,7 +7,8 @@ require'nvim-treesitter.configs'.setup {
       "bash",
       "c_sharp",
       "html",
-      "yaml"
+      "css",
+      "yaml",
   },
   sync_install = false,
   highlight = {
@@ -16,7 +17,7 @@ require'nvim-treesitter.configs'.setup {
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = true,
     patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
         -- For all filetypes
         -- Note that setting an entry here replaces all other patterns for this entry.
@@ -54,3 +55,11 @@ require'nvim-treesitter.configs'.setup {
     },
   },
 }
+-- Inject proper languages in html file
+require("vim.treesitter.query").set_query("php", "injections", [[
+; inherits: html
+
+(text) @html
+
+(comment) @phpdoc
+]])
