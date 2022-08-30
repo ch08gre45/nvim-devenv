03bd74e251276cc2a0c25263ec4327cc1c0bb0bc
ch08gre45 <drazvan85@gmail.com>
2022-08-30 10:31:32 +0300

    Add plugin folder to lua files sync

--- nvim/sync_vimrc.sh
+++ nvim/sync_vimrc.sh
@@ -3,10 +3,15 @@
 THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 mkdir -p ~/.config/nvim/lua
-# Remove old lua config files
+mkdir -p ~/.config/nvim/plugin
+# Remove old config files
 rm -rf ~/.config/nvim/lua/* 
+rm -rf ~/.config/nvim/plugin/* 
+
 # Update new lua configs
 cp -rf ${THIS_DIR}/lua/* ~/.config/nvim/lua/
+cp -rf ${THIS_DIR}/plugin/* ~/.config/nvim/plugin/
+
 # Update init.vim and .vimrc
 cp -f ${THIS_DIR}/init.vim ~/.config/nvim/init.vim
 cp -f ${THIS_DIR}/vimrc ~/.vimrc
