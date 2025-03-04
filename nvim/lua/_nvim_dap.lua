5ba71cee18b0db0a2160c81467681a25ed59d202
ch08gre45 <drazvan85@gmail.com>
2025-03-05 00:50:05 +0200

    Add dotnet launchsettings

--- nvim/lua/_nvim_dap.lua
+++ nvim/lua/_nvim_dap.lua
@@ -11,6 +11,22 @@ dap.configurations.cs = {
     type = "coreclr",
     name = "launch - netcoredbg",
     request = "launch",
+    env = function ()
+        local launchSettings = vim.fs.joinpath(vim.fn.getcwd(), "Properties", "launchSettings.json")
+
+        local stat = vim.uv.fs_stat(launchSettings)
+        if stat == nil then return nil end
+
+        local success, result = pcall(vim.fn.json_decode, vim.fn.readfile(launchSettings, ""))
+        if not success then return nil, "Error parsing JSON: " .. result end
+
+        local profiles = vim.tbl_keys(result.profiles)
+        local profileName = vim.fn.input('Profile name:', profiles[1])
+        print("Chosen" .. profileName)
+        local profile = result.profiles[profileName]
+        print("exit " .. vim.inspect(profile))
+        return profile.environmentVariables
+    end,
     program = function()
         return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
     end,
